c75202eac0d62338568e16c4b87890ae94cf0294
Administrator <Administrator@GDB8MCDKXR88P6N>
2019-08-19 09:38:11 +0800

    添加项目文件。

---
+++ EDC/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace EDC.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ EDC/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EDC.Startup))]
+namespace EDC
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
